8238a12b641f537d71c85620ec0c2e11a0067e73
suseda <101461969+suseda@users.noreply.github.com>
2023-06-12 23:50:30 +0300

    Update chat.dart

--- lib/chat.dart
+++ lib/chat.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/cupertino.dart';
-import 'package:firebase_auth/firebase_auth.dart' as auth;
-import 'friends_menu_widget.dart' as app;
+import 'friends_menu_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class Chat extends StatefulWidget {
-  final app.User selectedFriend;
 
+class Chat extends StatefulWidget {
+
+  final User selectedFriend;
   const Chat({required this.selectedFriend, Key? key}) : super(key: key);
 
   @override
@@ -17,8 +17,10 @@ class _ChatState extends State<Chat> {
   TextEditingController _textEditingController = TextEditingController();
   String _displayText = '';
   final List<String> messages = <String>[];
-  late app.User selectedFriend;
-  late app.User currentUser;
+  late User selectedFriend = widget.selectedFriend;
+  late User currentUser;
+  late CollectionReference userCollection;
+
 
   @override
   void dispose() {
@@ -27,53 +29,54 @@ class _ChatState extends State<Chat> {
   }
 
   @override
-  void initState() {
-    super.initState();
-    fetchCurrentUser();
-    selectedFriend = widget.selectedFriend;
-  }
+void initState() {
+  super.initState();
+  fetchCurrentUser();
+}
 
-  void fetchCurrentUser() async {
-    auth.User? user = auth.FirebaseAuth.instance.currentUser;
-    if (user != null) {
-      DocumentSnapshot currentUserDoc = await FirebaseFirestore.instance
-          .collection('users')
-          .doc(user.uid)
-          .get();
-      Map<String, dynamic> currentUserData =
-          currentUserDoc.data() as Map<String, dynamic>;
-      setState(() {
-        currentUser = app.User.fromJson(currentUserData);
-      });
-    }
-  }
+void fetchCurrentUser() async {
+  final currentUserDoc = await FirebaseFirestore.instance
+      .collection('users')
+      .doc('DHbAbJcnjHrXU6gjOvLW')
+      .get();
+  final currentUserData = currentUserDoc.data() as Map<String, dynamic>;
+  setState(() {
+    currentUser = User.fromJson(currentUserData);
+  });
+}
 
   void _handleButtonClick() async {
-    if (currentUser == null) {
-      return;
-    }
-    setState(() {
-      _displayText = _textEditingController.text;
-      messages.insert(messages.length, _displayText);
-      _textEditingController.clear();
-    });
+  setState(() {
+    _displayText = _textEditingController.text;
+    messages.insert(messages.length, _displayText);
+    _textEditingController.clear();
+  });
 
-    selectedFriend.messages.putIfAbsent(currentUser.name, () => []);
-    selectedFriend.messages[currentUser.name]!.add(_displayText);
+  selectedFriend.messages.putIfAbsent(currentUser.name, () => []);
+  selectedFriend.messages[currentUser.name]!.add(_displayText);
 
-    currentUser.messages.putIfAbsent(selectedFriend.name, () => []);
-    currentUser.messages[selectedFriend.name]!.add(_displayText);
+  currentUser.messages.putIfAbsent(selectedFriend.name, () => []);
+  currentUser.messages[selectedFriend.name]!.add(_displayText);
 
-    await FirebaseFirestore.instance
-        .collection('users')
-        .doc(selectedFriend.id)
-        .update({'messages': selectedFriend.messages});
+  final Map<String, dynamic> updatedData = {
+    'messages': selectedFriend.messages,
+  };
 
-    await FirebaseFirestore.instance
-        .collection('users')
-        .doc(currentUser.id)
-        .update({'messages': currentUser.messages});
-  }
+  await FirebaseFirestore.instance
+      .collection('users')
+      .doc(selectedFriend.id)
+      .update(updatedData);
+
+  final Map<String, dynamic> currentUserData = {
+    'messages': currentUser.messages,
+  };
+
+  await FirebaseFirestore.instance
+      .collection('users')
+      .doc('DHbAbJcnjHrXU6gjOvLW')
+      .update(currentUserData);
+}
+
 
   @override
   Widget build(BuildContext context) {
@@ -107,22 +110,20 @@ class _ChatState extends State<Chat> {
             children: [
               Expanded(
                 child: ListView.builder(
-                  itemCount:
-                      selectedFriend.messages[currentUser.name]?.length ?? 0,
-                  itemBuilder: (BuildContext context, int index) {
-                    final message =
-                        selectedFriend.messages[currentUser.name]![index];
-                    return Container(
-                      height: 50,
-                      margin: EdgeInsets.all(2),
-                      color: Colors.cyan.shade100,
-                      child: Text(
-                        '${currentUser.name} : $message',
-                        style: TextStyle(fontSize: 18),
-                      ),
-                    );
-                  },
-                ),
+                itemCount: selectedFriend.messages[currentUser.name]?.length ?? 0,
+                itemBuilder: (BuildContext context, int index) {
+                  final message = selectedFriend.messages[currentUser.name]![index];
+                  return Container(
+                    height: 50,
+                    margin: EdgeInsets.all(2),
+                    color: Colors.cyan.shade100,
+                    child: Text('${currentUser.name} : $message',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  );
+                },
+              ),
+
               ),
               Container(
                 color: Colors.white,
